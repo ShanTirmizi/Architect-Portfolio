2be7a29512eb62671820e9ab2972ac07f95b8bc3
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-02-06 18:05:03 +0000

    Finihsed about me page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,11 @@ const About = () => {
     return (
         <div className='about'>
             <img className='about__img' src={Architect} alt='My pic' />
-            <div>
+            <div className='about__img__content'>
                 <h1>About me</h1>
-                <h3>A graduate from the University of Portsmouth school of Architecture, looking for a job as a Part 1 Architectural Assistant.
-                    This portfolio showcases some of the work that I have carried out over the last few years at University and highlights some of my interests.</h3>
+                <hr />
+                <h3>A graduate from the University of Portsmouth school of Architecture, looking for a job as a Part 1 Architectural Assistant.</h3>
+                <h3>This portfolio showcases some of the work that I have carried out over the last few years at University and highlights some of my interests.</h3>
             </div>
         </div>
     )
